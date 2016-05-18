56ec464b0e4217baa3b37201b014f4c386f0cf4d
Miguel Puig <miguelpuig@loveflutter.com>
2016-05-18 20:22:05 +0100

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name flickrFeedApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the flickrFeedApp
+ */
+angular.module('flickrFeedApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name flickrFeedApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the flickrFeedApp
+ */
+angular.module('flickrFeedApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
